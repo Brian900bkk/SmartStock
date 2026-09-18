270106db60587443bb9c79d8c7b924a69568261b
Brian900bkk <briankiptui07@gmail.com>
2026-09-18 10:39:59 +0300

    Connect frontend to live API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smartstock-api-8mqb.onrender.com/api",
 });
 
 // Add JWT token to every request
