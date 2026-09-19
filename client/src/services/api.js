f7abfb69b28505883eee3db4d22057ba13ab2e65
Brian900bkk <briankiptui07@gmail.com>
2026-09-19 15:46:14 +0300

    Use Render API URL for frontend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://smartstock-api-8mqb.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
-
 // Add JWT token to every request
 api.interceptors.request.use(
   (config) => {
